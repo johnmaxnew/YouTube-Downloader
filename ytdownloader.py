311a3c6620469b396b97371e43276ed4e1044ffd
johnmaxnew <forevermeticulous@gmail.com>
2021-11-21 23:53:55 +0300

    Added pytube module

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,9 +1,10 @@
 from pytube import YouTube
 
 while True:
-    link = input("enter url: ")
+    link = input("enter url: ") 
     video = YouTube(link)
     for stream in video.streams.filter(only_audio = True, mime_type = "audio/mp4"):
         stream = video.streams.get_by_itag(140)
-        stream.download(output_path="/Downloads")
+        stream.download(output_path="Your directory here e.g '/Users/ADMIN/Desktop/music' ")
     print("done")
+    
