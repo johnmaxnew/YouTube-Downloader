86b6e08e8b6b754a8502ecc3e0ac1d54c1e70289
johnmaxnew <forevermeticulous@gmail.com>
2021-11-25 23:45:39 +0300

    Modified output type

--- ytdownloader.py
+++ ytdownloader.py
@@ -5,6 +5,6 @@ while True:
     video = YouTube(link)
     for stream in video.streams.filter(only_audio = True, mime_type = "audio/mp4"):
         stream = video.streams.get_by_itag(140)
-        stream.download(output_path="Your directory here e.g '/Users/ADMIN/Desktop/music' ")
+        stream.download(output_path= "Your directory here e.g '/Users/ADMIN/Desktop/music'")
     print("done")
     
